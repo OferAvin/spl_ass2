b03af9f4c8a7febc5d0bc9d13a252c37a576a7d6
spl211 <shilonyt@post.bgu.ac.il>
2020-12-09 17:27:44 +0200

    Input class

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,11 +1,18 @@
 package bgu.spl.mics.application;
+import com.google.gson.*;
 
-/** This is the Main class of the application. You should parse the input file, 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.File;
+
+/** This is the Main class of the application. You should parse the input file,
  * create the different components of the application, and run the system.
  * In the end, you should output a JSON.
  */
 public class Main {
 	public static void main(String[] args) {
-		
+
 	}
 }
